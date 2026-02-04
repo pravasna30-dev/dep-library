fe04b1c1720e7c14f15e8c0f91e881c5feca1f66
Prasanna Vasudevan <prasanna_vasudevan@apple.com>
2026-02-03 20:50:37 -0800

    Initial commit: library v1.0.0
    - UserService with findById(Long), findAll(), createUser()
    - User class with Long getId(), getEmail(), getName()
    - Ready to publish to mavenLocal

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "com.example"
+version = project.findProperty("version")?.toString() ?: "1.0.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    withSourcesJar()
+}
+
+repositories {
+    mavenCentral()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set("Example Library")
+                description.set("A library demonstrating breaking change detection")
+            }
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
